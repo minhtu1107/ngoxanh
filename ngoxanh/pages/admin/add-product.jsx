2e48f43365afa78cd2686f51d8b3ba3efb7543eb
minhtu1107 <minhtu1107@yahoo.com>
2021-09-04 20:54:07 +0700

    -update admin

--- ngoxanh/pages/admin/add-product.jsx
+++ ngoxanh/pages/admin/add-product.jsx
@@ -13,6 +13,9 @@ import React, { Component, useState } from 'react';
 const ReactQuill = typeof window === 'object' ? require('react-quill') : () => false;
 import 'react-quill/dist/quill.snow.css';
 
+import 'react-dropzone-uploader/dist/styles.css';
+import Dropzone from 'react-dropzone-uploader';
+
 import AdminHeader from '../../components/admin/header';
 
 export async function getServerSideProps(context) {
@@ -42,6 +45,18 @@ const AddProduct = (props) => {
     setDesc(content);
   }
 
+  // specify upload params and url for your files
+  const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+  
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => { console.log("status, meta, file"); console.log(status, meta, file) }
+  
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
+
   return (
     <div>
       <AdminHeader user={props.user}/>
@@ -66,12 +81,40 @@ const AddProduct = (props) => {
             <tr>
               <td>Hình ảnh</td>
               <td>
-                <input
-                  // className='search-header'
-                  type='file'
-                  id='product-image'
-                  placeholder=''
-                  name='product-image' 
+                <Dropzone
+                  // getUploadParams={getUploadParams}
+                  onChangeStatus={handleChangeStatus}
+                  // onSubmit={handleSubmit}
+                  maxFiles={5}
+                  accept="image/*"
+                  inputContent="Drag Files or Click to Browse (Up to 5 files)"
+                  styles={{
+                    dropzone: {
+                      overflow: 'auto',
+                      height: 150,
+                      flexDirection: 'row',
+                      alignItems: 'unset',
+                      display: 'block'
+                    },
+                    dropzoneActive: { borderColor: 'green' },
+                    previewImage: {
+                      maxHeight: 100,
+                      maxWidth: 'unset'
+                    },
+                    preview: {
+                      justifyContent: 'space-evenly',
+                      minHeight: 'unset',
+                      height: 120, width: 180,
+                      float: 'left',
+                      padding: '40px 1%'
+                    },
+                    inputLabelWithFiles: {
+                      alignSelf: 'center',
+                      margin: '45px 0px 0px 0px',
+                      width: '100px',
+                      float: 'left'
+                    }
+                  }}
                 />
               </td>
             </tr>
