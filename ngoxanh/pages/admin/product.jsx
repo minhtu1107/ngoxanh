5fcb27a63f09b03d338dae44677c0b719449096b
minhtu1107 <minhtu1107@yahoo.com>
2021-09-11 18:27:22 +0700

    -update admin

--- ngoxanh/pages/admin/product.jsx
+++ ngoxanh/pages/admin/product.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Router, { useRouter } from 'next/router';
 import { redirectTo } from '../../services/util';
 import { getSessionFromContext } from '../../services/auth';
+import { Close } from '@material-ui/icons';
+
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -20,6 +22,7 @@ import Dropzone from 'react-dropzone-uploader';
 
 import AdminHeader from '../../components/admin/header';
 import { createProduct } from '../../services/product';
+import ReactLoading from 'react-loading';
 
 export async function getServerSideProps(context) {
   const user = await getSessionFromContext(context);
@@ -30,6 +33,8 @@ export async function getServerSideProps(context) {
   let ret = {
     props: {
       user,
+      secondImage: [],
+      image:null,
     }
   };
   const id = parseInt(context.query.pid) || -1;
@@ -41,6 +46,12 @@ export async function getServerSideProps(context) {
     if(temp.data.data.length>0) {
       console.log(temp.data.data[0]);
       ret.props.product = temp.data.data[0];
+      if(ret.props.product.secondary_images != null) {
+        ret.props.secondImage = JSON.parse(ret.props.product.secondary_images);
+      }
+      if(ret.props.product.image != null) {
+        ret.props.image = ret.props.product.image;
+      }
     }
   }
 
@@ -49,9 +60,15 @@ export async function getServerSideProps(context) {
 
 const AddProduct = (props) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [mainImage, setMainImage] = useState([]);
   const [name, setName] = useState(props.product!=undefined?props.product.name:'');
   const [desc, setDesc] = useState(props.product!=undefined?props.product.description:'');
   const [shortDesc, setShortDesc] = useState(props.product!=undefined?props.product.short_description:'');
+  const [currentMainImage, setCurrentMainImage] = useState(props.image!=null?props.image:null);
+  const [currentSecondImage, setCurrentSecondImage] = useState(props.secondImage);
+  const [deleteImages, setDeleteImages] = useState([]);
+
   const onShortDescChange = (content) => {
     console.log('onChange', content);
     setShortDesc(content);
@@ -66,12 +83,20 @@ const AddProduct = (props) => {
   const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
   
   // called every time a file's `status` changes
-  const handleChangeStatus = ({ meta, file }, status) => { console.log("status, meta, file"); console.log(status, meta, file) }
+  const handleChangeStatus = ({ meta, file }, status, allFiles) => { 
+    console.log("status, meta, file"); 
+    console.log(status, meta, allFiles) 
+    setImages(allFiles);
+  }
   
   // receives array of files that are done uploading when submit button is clicked
   const handleSubmit = (files, allFiles) => {
     console.log(files.map(f => f.meta))
     allFiles.forEach(f => f.remove())
+  }
+
+  const handleMainImageChangeStatus = ({ meta, file }, status, allFiles) => { 
+    setMainImage(allFiles);
   }
 
   const handleProductName = (e) => {
@@ -88,19 +113,133 @@ const AddProduct = (props) => {
       body['desc'] = desc;
       body['shortDesc'] = shortDesc;
 
+      if(mainImage.length>0) {
+        body['mainImage'] = mainImage;
+      }
+
+      if(images.length>0) {
+        body['images'] = images;
+        // console.log('body.images');
+        // console.log(body.images);
+      }
+
+      if(currentMainImage=='deleted') {
+        body['currentMainImage'] = 'deleted';
+      }
+
+      if(currentSecondImage.length>0) {
+        body['secondImages'] = currentSecondImage;
+      }
+
+      if(deleteImages.length>0) {
+        body['deleteImages'] = deleteImages;
+      }
+
+      if(props.product!=undefined) {
+        body['pid'] = props.product.id;
+      }
+
       setIsLoading(true);
       createProduct(body)
       .then((res) => {
         console.log(res.data);
-        Router.push('/admin/product-management');
+        // Router.push('/admin/product-management');
       })
       .catch((err) => {
+        console.log('res.data');
         console.log(err);
       })
       .finally(()=> {
         setIsLoading(false);
       })
     }
+  }
+
+  const handleDeleteMainImage = (img) => {
+    setDeleteImages([...img]);
+    setCurrentMainImage('deleted');
+  }
+
+  const handleDeleteSecondImage = (img) => {
+    console.log("handleDeleteSecondImage bf ", deleteImages)
+    setDeleteImages([...deleteImages, img]);
+    let temp = currentSecondImage.filter(ii=>ii!==img);
+    console.log("handleDeleteSecondImage ", deleteImages)
+    setCurrentSecondImage(temp);
+  }
+
+  const renderCurrentImage = (currentList, handleDelete) => {
+    console.log('renderCurrentImage ' + currentList);
+    return (
+      currentList.length > 0 ? currentList.map(img => (
+        <div key={img}>
+          <div className="dzu-previewContainer" 
+          style={{
+            justifyContent: 'space-evenly',
+            minHeight: 'unset',
+            height: 120,
+            width: 180,
+            float: 'left',
+            padding: '40px 1%'
+          }}>
+            <img className="dzu-previewImage"
+              style={{
+                maxHeight: 100,
+                maxWidth: 'unset'
+              }} src={img} />
+            <div className="dzu-previewStatusContainer" style={{cursor:'pointer'}}>
+              <Close onClick={()=>handleDelete(img)}/>
+            </div>
+          </div>
+        </div>
+      )) : (<></>)
+    )
+  }
+
+  const renderUploadImage = (maxUpload, handleUpload) => {
+    let inputContentMsg = 'Drag Files or Click to Browse';
+    if(maxUpload > 1) {
+      inputContentMsg += ` (Up to ${maxUpload} files)`;
+    }
+    return (
+      maxUpload > 0 ? (
+        <Dropzone
+          // getUploadParams={getUploadParams}
+          onChangeStatus={handleUpload}
+          // onSubmit={handleSubmit}
+          maxFiles={maxUpload}
+          accept="image/*"
+          inputContent={inputContentMsg}
+          styles={{
+            dropzone: {
+              overflow: 'auto',
+              height: 150,
+              flexDirection: 'row',
+              alignItems: 'unset',
+              display: 'block'
+            },
+            dropzoneActive: { borderColor: 'green' },
+            previewImage: {
+              maxHeight: 100,
+              maxWidth: 'unset'
+            },
+            preview: {
+              justifyContent: 'space-evenly',
+              minHeight: 'unset',
+              height: 120, width: 180,
+              float: 'left',
+              padding: '40px 1%'
+            },
+            inputLabelWithFiles: {
+              alignSelf: 'center',
+              margin: '45px 0px 0px 0px',
+              width: '100px',
+              float: 'left'
+            }
+          }}
+        />
+      ) : (<></>)
+    )
   }
 
   return (
@@ -127,9 +266,71 @@ const AddProduct = (props) => {
               </td>
             </tr>
             <tr>
-              <td>Hình ảnh</td>
+              <td>Ảnh đại diện</td>
               <td>
+                {/* <div>
+                    <div className="dzu-previewContainer" style={{
+                      justifyContent:'space-evenly',
+                      minHeight: 'unset',
+                      height:120,
+                      width:180,
+                      float:'left',
+                      padding: '40px 1%'
+                    }}>
+                      <img className="dzu-previewImage" 
+                      style={{maxHeight: 100,
+                      maxWidth:'unset'}}src={props.product.image} />
+                      <div className="dzu-previewStatusContainer">
+                        <Close/>
+                      </div>
+                    </div>
+                </div>
                 <Dropzone
+                  // getUploadParams={getUploadParams}
+                  onChangeStatus={handleMainImageChangeStatus}
+                  // onSubmit={handleSubmit}
+                  maxFiles={1}
+                  accept="image/*"
+                  inputContent="Drag Files or Click to Browse"
+                  styles={{
+                    dropzone: {
+                      overflow: 'auto',
+                      height: 150,
+                      flexDirection: 'row',
+                      alignItems: 'unset',
+                      display: 'block'
+                    },
+                    dropzoneActive: { borderColor: 'green' },
+                    previewImage: {
+                      maxHeight: 100,
+                      maxWidth: 'unset'
+                    },
+                    preview: {
+                      justifyContent: 'space-evenly',
+                      minHeight: 'unset',
+                      height: 120, width: 180,
+                      float: 'left',
+                      padding: '40px 1%'
+                    },
+                    inputLabelWithFiles: {
+                      alignSelf: 'center',
+                      margin: '45px 0px 0px 0px',
+                      width: '100px',
+                      float: 'left'
+                    }
+                  }}
+                /> */}
+                {(currentMainImage != null && currentMainImage !== 'deleted')
+                  ? renderCurrentImage([currentMainImage], handleDeleteMainImage)
+                  : renderUploadImage(1, handleMainImageChangeStatus)}
+              </td>
+            </tr>
+            <tr>
+              <td>Ảnh minh họa</td>
+              <td>
+                {renderCurrentImage(currentSecondImage, handleDeleteSecondImage)}
+                {renderUploadImage(5-currentSecondImage.length, handleChangeStatus)}
+                {/* <Dropzone
                   // getUploadParams={getUploadParams}
                   onChangeStatus={handleChangeStatus}
                   // onSubmit={handleSubmit}
@@ -163,7 +364,7 @@ const AddProduct = (props) => {
                       float: 'left'
                     }
                   }}
-                />
+                /> */}
               </td>
             </tr>
             <tr>
@@ -184,6 +385,9 @@ const AddProduct = (props) => {
           <Button variant="primary" onClick={editProduct}>Lưu</Button>
         </div>
       </div>
+      {isLoading && (<div className='loading'>
+        <ReactLoading type='spinningBubbles' color='#0d6efd' />
+      </div>)}
     </div>
   )
 }
--- ngoxanh/services/product.jsx
+++ ngoxanh/services/product.jsx
@@ -39,6 +39,40 @@ const createProduct = (body) => {
   formData.append('desc', body.desc);
   formData.append('shortDesc', body.shortDesc);
 
+  if(body.mainImage) {
+    formData.append('mainImage', body.mainImage[0].file);
+    console.log(body.mainImage);
+  }
+
+  if(body.currentMainImage) {
+    formData.append('deleteMainImage', 'deleted');
+  }
+
+  if(body.images) {
+    body.images.forEach(f => {
+      // console.log(f.file);
+      formData.append('images[]', f.file);
+    });
+  }
+
+  if(body.secondImages) {
+    body.secondImages.forEach(f => {
+      // console.log(f.file);
+      formData.append('secondImages[]', f);
+    });
+  }
+
+  if(body.deleteImages) {
+    body.deleteImages.forEach(f => {
+      // console.log(f.file);
+      formData.append('deleteImages[]', f);
+    });
+  }
+
+  if(body.pid) {
+    formData.append('pid', body.pid);
+  }
+
   const params = {
     method: 'post',
     url: 'https://ngoxanh.000webhostapp.com/api/createProduct',
